6b33285b1d7c179d89ae818cf2c63020f8923f4f
Ezra-code <njerukithinji872@gmail.com>
2022-05-25 07:16:19 +0300

    add javascript validation to the project

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,12 +1,30 @@
-
+let inputEl = document.getElementById('input-el')
+let displayEl = document.getElementById('display-el')
+let age = document.getElementById('age')
+let errorTxt = document.getElementById('error-txt')
+let errorEl =document.getElementById('error-el')
 function ageInDays(){
-
+    if(inputEl.value == ''){
+        errorTxt.style.display = 'block'
+    }else if(isNaN(inputEl.value) || inputEl.value > 2022){
+        errorEl.style.display = 'block'
+    }else{
+        let birthYear = inputEl.value
+        yearsInDays = (2022 - birthYear) * 365
+        console.log(yearsInDays)
+        age = yearsInDays
+        displayEl.style.display ='block'
+        
+    }
 }
 
 function reset() {
     document.getElementById('age').remove()
 }
-
+inputEl.addEventListener('keypress', () => {
+    errorTxt.style.display = 'none'
+    errorEl.style.display = 'none'
+})
 // function rps() {
 //     let choice = "paper"
 //     let value = ['Rock', 'paper', 'scissors']
